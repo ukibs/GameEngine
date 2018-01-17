6fd382c84a20e99c0d58fbb20d2718a184e3e6ba
Aritz Valencia <aritzondovq@msn.com>
2018-01-17 13:36:57 +0100

    engineManager

---
+++ GameEngine/GameEngine/EngineManager.cpp
@@ -0,0 +1,60 @@
+#include "EngineManager.h"
+
+
+
+EngineManager::EngineManager()
+{
+}
+
+
+EngineManager::~EngineManager()
+{
+}
+
+bool EngineManager::start()
+{
+	//Create the managers
+	
+	TimerManager::CreateSingleton();
+	InputManager::CreateSingleton();
+	ActionManager::CreateSingleton();
+	ColliderManager::CreateSingleton();
+	ObjectManager::CreateSingleton();
+	RenderManager::CreateSingleton();
+	
+	
+	//Initialize SDL
+	if (!RenderManager::GetInstance().init())
+	{
+		printf("Failed to initialize!\n");
+		return false;
+	}
+	return true;
+}
+
+bool EngineManager::update()
+{
+	TimerManager::GetInstance().update();
+	ActionManager::GetInstance().update();
+	RenderManager::GetInstance().preUpdate();
+	ObjectManager::GetInstance().update();
+	RenderManager::GetInstance().postUpdate();
+	
+	return InputManager::GetInstance().checkQuit();;
+}
+
+void EngineManager::close()
+{
+	RenderManager::GetInstance().close();
+	RenderManager::DestroySingleton();
+	ObjectManager::GetInstance().close();
+	ObjectManager::DestroySingleton();
+	ColliderManager::GetInstance().close();
+	ColliderManager::DestroySingleton();
+	ActionManager::GetInstance().close();
+	ActionManager::GetInstance();
+	InputManager::GetInstance().close();
+	InputManager::DestroySingleton();
+	TimerManager::GetInstance().close();
+	TimerManager::DestroySingleton();
+}
---
+++ GameEngine/GameEngine/EngineManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include "Singleton.h"
+#include "RenderManager.h"
+#include "ObjectManager.h"
+#include "ActionManager.h"
+#include "InputManager.h"
+#include "ColliderManager.h"
+#include "TimerManager.h"
+#include "Text.h"
+using namespace std;
+
+class EngineManager : public Singleton<EngineManager>
+{
+public:
+	EngineManager();
+	~EngineManager();
+	bool start();
+	bool update();
+	void close();
+};
+
